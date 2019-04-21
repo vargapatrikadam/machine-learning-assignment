a38bdd2ae71762a1936c7912f6fa86f2b1f985aa
Patrik Varga <patrikadamvarga@gmail.com>
2019-04-21 13:25:09 +0200

    k-means & agglomerative clustering algorithms added result sets intersected

--- main.py
+++ main.py
@@ -24,15 +24,55 @@ iris_dataset = pd.read_csv('data/iris_dataset.txt',
                                   'class'])
 X = iris_dataset.drop('class',axis=1)
 
+#Dimenzióredukció végrehajtása az adathalmazon
 from sklearn.decomposition import PCA
 
 pca = PCA(n_components=2)
 pca.fit(X)
 pcaMapping = pca.transform(X)
 
+#A dimenzióredukció eredményének vizualizációja
 import matplotlib.pyplot as plt
 plt.scatter(pcaMapping[:,0],pcaMapping[:,1])
 plt.title('Iris dataset visualization')
 plt.show()
 plt.clf()
 
+#KMeans algoritmus használata az adathalmazon klaszterezésként
+from sklearn.cluster import KMeans
+
+K = 3
+kmeans = KMeans(n_clusters=K).fit(X)
+
+for i in range(0,K):
+    plt.scatter(pcaMapping[kmeans.labels_ == i][:, 0],
+                pcaMapping[kmeans.labels_ == i][:, 1])
+plt.title('K-Means eredmény')
+plt.show()
+plt.clf()
+
+from sklearn.cluster import AgglomerativeClustering
+agglomerative = AgglomerativeClustering(n_clusters=K).fit(X)
+
+for i in range(0,K):
+    plt.scatter(pcaMapping[agglomerative.labels_ == i][:, 0],
+                pcaMapping[agglomerative.labels_ == i][:, 1])
+plt.title('Agglomerációs eredmény')
+plt.show()
+plt.clf()
+
+def clusterIntersection(cluster1, cluster2):
+    result = 0
+    for c1 in cluster1:
+        for c2 in cluster2:
+            if (c1 - c2).sum() == 0:
+                result += 1
+                continue
+    return result
+
+for kmeansIndex in set(kmeans.labels_):
+    for agglomerativeIndex in set(agglomerative.labels_):
+        print(kmeansIndex,',',agglomerativeIndex,',',
+        clusterIntersection(
+            X[kmeans.labels_ == kmeansIndex].values,
+            X[agglomerative.labels_ == agglomerativeIndex].values))
