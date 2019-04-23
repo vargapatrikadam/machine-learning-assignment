7953b9f7fb9aae1196cd61d9d41d3bb31463d196
Patrik Varga <patrikadamvarga@gmail.com>
2019-04-23 19:43:03 +0200

    data scaled with standard scaler decision tree

--- classification.py
+++ classification.py
@@ -22,24 +22,55 @@ iris_dataset = pd.read_csv('data/iris_dataset.txt',
                                   'petal_len',
                                   'petal_wid',
                                   'class'])
+
 X = iris_dataset.drop('class',axis=1)
 Y = iris_dataset['class']
 
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+#Standard scaler alkalmazása az adatokon
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler().fit(X)
+scaled = scaler.transform(X)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y)
+#Train és test splitting
+from sklearn.model_selection import train_test_split, cross_val_score
 
-#GridSearchCV
+X_train, X_test, Y_train, Y_test = train_test_split(scaled,Y)
+
+#GridSearchCV with MLPClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPClassifier
 
+#Setting up parameters for the GridSearch with the MLP Classifier
 mlpParameters = {
-    'hidden_layer_sizes':[(3,4,5),(10,20,10),(10,5,2),(5,10,20,25,30,15,6)],
-    'activation':('tanh','relu','logistic'),
+    #'hidden_layer_sizes':[(2,4,2),(5,6,8,5),(10,20,10,6)],
+    #'hidden_layer_sizes':[(2,4,2),(5,6,8)],
+    'hidden_layer_sizes':[(90,),(80,),(2,4,3),(5,6,3),(4,)],
+    'activation':('tanh','relu'),
     'solver':('sgd','adam'),
     'learning_rate':('constant','adaptive')
 }
-mlp = MLPClassifier()
-gridSearch = GridSearchCV(mlp,mlpParameters, cv=5)
-gridSearch.fit(X_train,Y_train)
-gridSearch.score(X_test, Y_test)
+
+#Executing the grid search
+clf = GridSearchCV(MLPClassifier(max_iter=10000),mlpParameters, cv=10, scoring='accuracy')
+clf.fit(X_train,Y_train)
+
+# Best parameter set
+print('MLP Classifier best parameters found:\n', clf.best_params_)
+print('MLP CLassifier best score:\n',clf.best_score_)
+
+#Printing the reports on the best set tested with the test set
+from sklearn.metrics import classification_report
+print('MLP Classifier results on the test set:')
+print(classification_report(Y_test, clf.predict(X_test)))
+
+#Setting up the decision tree for the problem
+from sklearn import tree
+
+decisionTree = tree.DecisionTreeClassifier().fit(X_train,Y_train)
+
+#Printing out the score of the decision tree
+print('Decision tree score:\n',decisionTree.score(X_train,Y_train))
+
+#Printing out the results of the decision tree tested with the test set
+print('Decision tree results on the test set:')
+print(classification_report(Y_test, decisionTree.predict(X_test)))
