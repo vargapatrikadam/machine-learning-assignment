801d0f03f1ad6e5930d169a8954cce5e53a62168
Patrik Varga <patrikadamvarga@gmail.com>
2019-04-25 18:23:20 +0200

    modified clustering & PCA visualization

--- clustering.py
+++ clustering.py
@@ -14,6 +14,7 @@ Attribútumok:
 
 #Adathalmaz beolvasása
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 iris_dataset = pd.read_csv('data/iris_dataset.txt',
                            sep=',',
@@ -23,64 +24,62 @@ iris_dataset = pd.read_csv('data/iris_dataset.txt',
                                   'petal_wid',
                                   'class'])
 X = iris_dataset.drop('class',axis=1)
+X = StandardScaler().fit_transform(X)
+Y = iris_dataset['class']
 
 #Dimenzióredukció végrehajtása az adathalmazon
 from sklearn.decomposition import PCA
-
 pca = PCA(n_components=2)
-pca.fit(X)
-pcaMapping = pca.transform(X)
+principalComponents = pca.fit_transform(X)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+finalDf = pd.concat([principalDf, iris_dataset[['class']]], axis = 1)
 
 #A dimenzióredukció eredményének vizualizációja
 import matplotlib.pyplot as plt
-plt.scatter(pcaMapping[:,0],pcaMapping[:,1])
-plt.title('Iris dataset visualization')
+plt.figure(figsize = (6,6))
+plt.title('PCA az az Iris Dataset-en')
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['class'] == target
+    plt.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+plt.legend(targets)
+plt.grid()
 plt.show()
 plt.clf()
 
 #Keresett klaszterek számosságának beállítása
 K = 3
-
 #KMeans algoritmus használata az adathalmazon klaszterezésként
 from sklearn.cluster import KMeans
 
 kmeans = KMeans(n_clusters=K).fit(X)
 
 #KMeans eredmény megjelenítése scatter ploton
+plt.figure(figsize = (6,6))
 for i in range(0,K):
-    plt.scatter(pcaMapping[kmeans.labels_ == i][:, 0],
-                pcaMapping[kmeans.labels_ == i][:, 1])
+    plt.scatter(principalComponents[kmeans.labels_ == i][:, 0],
+                principalComponents[kmeans.labels_ == i][:, 1])
 plt.title('K-Means eredmény')
+plt.grid()
 plt.show()
 plt.clf()
 
 #DBSCAN algoritmus használata az adathalmazon
 from sklearn.cluster import DBSCAN
 
-dbscan = DBSCAN(eps=1,min_samples=10).fit(X)
+dbscan = DBSCAN(eps=1,min_samples=1).fit(X)
 
 #DBSCAN eredmény megjelenítése scatter ploton
+plt.figure(figsize = (6,6))
 for i in range(0,K):
-    plt.scatter(pcaMapping[dbscan.labels_ == i][:, 0],
-                pcaMapping[dbscan.labels_ == i][:, 1])
+    plt.scatter(principalComponents[dbscan.labels_ == i][:, 0],
+                principalComponents[dbscan.labels_ == i][:, 1])
 plt.title('DBSCAN eredmény')
+plt.grid()
 plt.show()
 plt.clf()
-
-#Klaszterek metszetének kiszámolása
-def clusterIntersection(cluster1, cluster2):
-    result = 0
-    for c1 in cluster1:
-        for c2 in cluster2:
-            if (c1 - c2).sum() == 0:
-                result += 1
-                continue
-    return result
-
-for kmeansIndex in set(kmeans.labels_):
-    for dbscanIndex in set(dbscan.labels_):
-        print(kmeansIndex,',',dbscanIndex,',',
-        clusterIntersection(
-            X[kmeans.labels_ == kmeansIndex].values,
-            X[dbscan.labels_ == dbscanIndex].values))
-
