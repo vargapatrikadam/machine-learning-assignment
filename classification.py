24eeba560f7e48164f116f1b05d69110941d091b
Patrik Varga <patrikadamvarga@gmail.com>
2019-04-27 00:35:33 +0200

    dataset replaced with more complex mushroom dataset

--- classification.py
+++ classification.py
@@ -1,43 +1,89 @@
 '''
-Osztályozási feladat az 'Iris Data Set'-en, az UCI Machine Learning Repository-ból.
-https://archive.ics.uci.edu/ml/datasets/Iris
+Klaszterezési feladat a 'Mushroom' data set-en, az UCI Machine Learning Repository-ból.
+https://archive.ics.uci.edu/ml/datasets/Mushroom
 Attribútumok:
-    1. sepal length in cm
-    2. sepal width in cm
-    3. petal length in cm
-    4. petal width in cm
-    5. class:
-    -- Iris Setosa
-    -- Iris Versicolour
-    -- Iris Virginica
+    1. cap-shape:                 bell=b,conical=c,convex=x,flat=f,
+                                  knobbed=k,sunken=s
+    2. cap-surface:               fibrous=f,grooves=g,scaly=y,smooth=s
+    3. cap-color:                 brown=n,buff=b,cinnamon=c,gray=g,green=r,
+                                  pink=p,purple=u,red=e,white=w,yellow=y
+    4. bruises?:                  bruises=t,no=f
+    5. odor:                      almond=a,anise=l,creosote=c,fishy=y,foul=f,
+                                  musty=m,none=n,pungent=p,spicy=s
+    6. gill-attachment:           attached=a,descending=d,free=f,notched=n
+    7. gill-spacing:              close=c,crowded=w,distant=d
+    8. gill-size:                 broad=b,narrow=n
+    9. gill-color:                black=k,brown=n,buff=b,chocolate=h,gray=g,
+                                  green=r,orange=o,pink=p,purple=u,red=e,
+                                  white=w,yellow=y
+    10. stalk-shape:              enlarging=e,tapering=t
+    11. stalk-root:               bulbous=b,club=c,cup=u,equal=e,
+                                  rhizomorphs=z,rooted=r,missing=?
+    12. stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    13. stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    14. stalk-color-above-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,
+                                  pink=p,red=e,white=w,yellow=y
+    15. stalk-color-below-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,
+                                  pink=p,red=e,white=w,yellow=y
+    16. veil-type:                partial=p,universal=u
+    17. veil-color:               brown=n,orange=o,white=w,yellow=y
+    18. ring-number:              none=n,one=o,two=t
+    19. ring-type:                cobwebby=c,evanescent=e,flaring=f,large=l,
+                                  none=n,pendant=p,sheathing=s,zone=z
+    20. spore-print-color:        black=k,brown=n,buff=b,chocolate=h,green=r,
+                                  orange=o,purple=u,white=w,yellow=y
+    21. population:               abundant=a,clustered=c,numerous=n,
+                                  scattered=s,several=v,solitary=y
+    22. habitat:                  grasses=g,leaves=l,meadows=m,paths=p,
+                                  urban=u,waste=w,woods=d
+    23. class:                    edible=e, poisonous=p
 '''
 
 #Adathalmaz beolvasása
 import pandas as pd
 
-iris_dataset = pd.read_csv('data/iris_dataset.txt',
+mushroom_dataset = pd.read_csv('data/agaricus-lepiota.txt',
                            sep=',',
-                           names=['sepal_len',
-                                  'sepal_wid',
-                                  'petal_len',
-                                  'petal_wid',
-                                  'class'])
+                           names=['class',
+                                  'cap-shape',
+                                  'cap-surface',
+                                  'cap-color',
+                                  'bruises',
+                                  'odor',
+                                  'gill-attachment',
+                                  'gill-spacing',
+                                  'gill-size',
+                                  'gill-color',
+                                  'stalk-shape',
+                                  'stalk-root',
+                                  'stalk-surface-above-ring',
+                                  'stalk-surface-below-ring',
+                                  'stalk-color-above-ring',
+                                  'stalk-color-below-ring',
+                                  'veil-type',
+                                  'veil-color',
+                                  'ring-number',
+                                  'ring-type',
+                                  'spore-print-color',
+                                  'population',
+                                  'habitat'])
+#Hiányzó adatokat tartalmazó rekordok eldobása
+mushroom_dataset = mushroom_dataset[mushroom_dataset['stalk-root'] != '?']
+#Oszlopok típusainak átkonvertálása
+for col in mushroom_dataset:
+    mushroom_dataset[col] = mushroom_dataset[col].astype('category')
+#Attribútumok leválasztása az osztálytól
+X = mushroom_dataset.drop('class',axis=1)
+cat_columns = X.select_dtypes(['category']).columns
+X[cat_columns] = X[cat_columns].apply(lambda x: x.cat.codes)
+Y = mushroom_dataset['class']
 
-#Adathalmazról az 'class' attrúbútum leválasztása Y változóba
-X = iris_dataset.drop('class',axis=1)
-Y = iris_dataset['class']
-
-#Standard scaler alkalmazása az adatokon
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler().fit(X)
-scaled = scaler.transform(X)
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 
 #Adatok szétválasztása tanító és tesztelő egységekre
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(scaled,Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3)
 
 #GridSearchCV és MLP Classifier beimportálása
-from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPClassifier
 
 #Decision Tree classifier beimportálása
@@ -56,90 +102,59 @@ from sklearn.metrics import classification_report, precision_score
 from datetime import datetime
 
 #Decision Tree beállítása
-decisionTreeParams = {}
-decisionTree = GridSearchCV(tree.DecisionTreeClassifier(), decisionTreeParams, cv=10, scoring='accuracy')
-print('Decision Tree tanítás alatt')
+decisionTree = tree.DecisionTreeClassifier(max_depth=3)
 start=datetime.now()
 decisionTree.fit(X_train,Y_train)
-#decisionTree = tree.DecisionTreeClassifier().fit(X_train,Y_train)
 print('Decision tree tanítási idő: ', (datetime.now()-start))
 
-#GridSearchCV beállítása a MLP Classifier-re
+'''
 mlpParameters = {
     #'hidden_layer_sizes':[(2,4,2),(5,6,8,5),(10,20,10,6)],
     #'hidden_layer_sizes':[(2,4,2),(5,6,8)],
-    'hidden_layer_sizes':[(90,),(80,),(2,4,3),(10,20,10),(4,)],
+    'hidden_layer_sizes':[(90,),(80,),(50,),(2,4,3),(10,20,10),(4,)],
     'activation':('tanh','relu'),
     'solver':('sgd','adam'),
     'learning_rate':('constant','adaptive')
 }
-clf = GridSearchCV(MLPClassifier(max_iter=10000),mlpParameters, cv=10, scoring='accuracy')
-print('MLP tanítás alatt')
+mlpClassifier = GridSearchCV(MLPClassifier(max_iter=10),mlpParameters, cv=10, scoring='accuracy')
+'''
+mlpClassifier = MLPClassifier(max_iter=10, hidden_layer_sizes=(50,),learning_rate='constant',solver='adam')
 start=datetime.now()
-clf.fit(X_train,Y_train)
+mlpClassifier.fit(X_train,Y_train)
 print('MLP tanítási idő: ', (datetime.now()-start))
 
-#GridSearchCV beállítása a Naive Bayes classifier-re
-gaussanparams = {}
-gaussian = GridSearchCV(GaussianNB(), gaussanparams, cv=10, scoring='accuracy')
-print('Naive Bayes tanítás alatt')
+gaussian = GaussianNB()
 start=datetime.now()
 gaussian.fit(X_train, Y_train)
 print('Naive Bayes tanítási idő: ', (datetime.now()-start))
 
-#GridSearchCV beállítása Support Vector Machine classifier-re
-supportVectorMachineParams = {'decision_function_shape':('ovo','ovr'),
-                              'gamma':('auto','scale')}
-supportVectorMachine = GridSearchCV(svm.SVC(),supportVectorMachineParams, cv=10,scoring='accuracy')
-print('Support Vector Machine tanítás alatt')
+supportVectorMachine = svm.SVC(kernel='linear',decision_function_shape='ovo', gamma='auto')
 start=datetime.now()
 supportVectorMachine.fit(X_train, Y_train)
 print('Support Vector Machine tanítási idő: ', (datetime.now()-start))
 
 
-#Decision Tree legjobb score
-print('Decision Tree legjobb score:\n',decisionTree.best_score_)
-#A Decision Tree teszteredményei
 print('Decision tree eredménye a test set-en:\n',classification_report(Y_test, decisionTree.predict(X_test)))
+scores = cross_val_score(decisionTree, X_test, Y_test, cv=5)
+print("Decision Tree pontosság: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-#Naive Bayes legjobb score
-print('Naive Bayes legjobb score:\n',gaussian.best_score_)
-#Naive Bayes teszteredményei
+
 print('Naive Bayes eredménye a test set-en:\n',classification_report(Y_test, gaussian.predict(X_test)))
+scores = cross_val_score(gaussian, X_test, Y_test, cv=5)
+print("Naive Bayes pontosság: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-#Legjobb paraméterű MLP Classifier a megadottak közül a GridSearch szerint
-print('MLP Classifier legjobb paraméter:\n', clf.best_params_)
-print('MLP CLassifier legjobb score:\n',clf.best_score_)
-#A legjobb eredménnyel rendelkező MLP Classifier-nek a teszteredményei
-print('MLP Classifier eredménye a test set-en:\n',classification_report(Y_test, clf.predict(X_test)))
 
-#Legjobb paraméterű Support Vector Machine a megadottak közül a GridSearch szerint
-print('Support Vector Machine legjobb paraméter:\n', supportVectorMachine.best_params_)
-print('Support Vector Machine legjobb score:\n',supportVectorMachine.best_score_)
-#A legjobb eredménnyel rendelkező Support Vector Machine-nek a teszteredményei
+print('MLP Classifier eredménye a test set-en:\n',classification_report(Y_test, mlpClassifier.predict(X_test)))
+scores = cross_val_score(mlpClassifier, X_test, Y_test, cv=5)
+print("MLP pontosság: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
 print('Support Vector Machine eredménye a test set-en:\n',classification_report(Y_test, supportVectorMachine.predict(X_test)))
+scores = cross_val_score(supportVectorMachine, X_test, Y_test, cv=5)
+print("Support Vector Machine pontosság: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-
-#Adatok rng-szer splitelése, ezáltal az osztályozók pontossága a teszt adatokkal a precision változókban egy mediánba
-rng = 10000
-print('Osztályozók tesztelése {} -szer'.format(rng))
-mlpPrecision = 0
-decisionTreePrecision = 0
-gaussianPrecision = 0
-supportVectorMachinePrecision = 0
-start=datetime.now()
-for i in range(rng):
-    X_train, X_test, Y_train, Y_test = train_test_split(scaled, Y)
-    mlpPrecision += precision_score(Y_test, clf.predict(X_test), average='weighted')
-    decisionTreePrecision += precision_score(Y_test, decisionTree.predict(X_test), average='weighted')
-    gaussianPrecision += precision_score(Y_test, gaussian.predict(X_test), average='weighted')
-    supportVectorMachinePrecision += precision_score(Y_test, supportVectorMachine.predict(X_test),average='weighted')
-mlpPrecision = mlpPrecision / rng
-decisionTreePrecision = decisionTreePrecision / rng
-gaussianPrecision = gaussianPrecision/ rng
-supportVectorMachinePrecision = supportVectorMachinePrecision / rng
-print('Osztályozók tesztelése ideje: {}'.format(datetime.now()-start))
-print('MLP Classifier eredménye %d iteráció után: %f' % (rng, mlpPrecision))
-print('Decision Tree eredménye %d iteráció után: %f' % (rng, decisionTreePrecision))
-print('Naive Bayes eredménye %d iteráció után: %f' % (rng, gaussianPrecision))
-print('Support Vector Machine eredménye %d iteráció után: %f' % (rng, supportVectorMachinePrecision))
+print('Osztályozók tesztelése')
+print('MLP Classifier eredménye: %f' % (precision_score(Y_test, mlpClassifier.predict(X_test), average='weighted')))
+print('Decision Tree eredménye: %f' % (precision_score(Y_test, decisionTree.predict(X_test), average='weighted')))
+print('Naive Bayes eredménye: %f' % (precision_score(Y_test, gaussian.predict(X_test), average='weighted')))
+print('Support Vector Machine eredménye: %f' % (precision_score(Y_test, supportVectorMachine.predict(X_test),average='weighted')))
--- clustering.py
+++ clustering.py
@@ -1,31 +1,82 @@
 '''
-Klaszterezési feladat az 'Iris Data Set'-en, az UCI Machine Learning Repository-ból.
-https://archive.ics.uci.edu/ml/datasets/Iris
+Klaszterezési feladat a 'Mushroom' data set-en, az UCI Machine Learning Repository-ból.
+https://archive.ics.uci.edu/ml/datasets/Mushroom
 Attribútumok:
-    1. sepal length in cm
-    2. sepal width in cm
-    3. petal length in cm
-    4. petal width in cm
-    5. class:
-    -- Iris Setosa
-    -- Iris Versicolour
-    -- Iris Virginica
+    1. cap-shape:                 bell=b,conical=c,convex=x,flat=f,
+                                  knobbed=k,sunken=s
+    2. cap-surface:               fibrous=f,grooves=g,scaly=y,smooth=s
+    3. cap-color:                 brown=n,buff=b,cinnamon=c,gray=g,green=r,
+                                  pink=p,purple=u,red=e,white=w,yellow=y
+    4. bruises?:                  bruises=t,no=f
+    5. odor:                      almond=a,anise=l,creosote=c,fishy=y,foul=f,
+                                  musty=m,none=n,pungent=p,spicy=s
+    6. gill-attachment:           attached=a,descending=d,free=f,notched=n
+    7. gill-spacing:              close=c,crowded=w,distant=d
+    8. gill-size:                 broad=b,narrow=n
+    9. gill-color:                black=k,brown=n,buff=b,chocolate=h,gray=g,
+                                  green=r,orange=o,pink=p,purple=u,red=e,
+                                  white=w,yellow=y
+    10. stalk-shape:              enlarging=e,tapering=t
+    11. stalk-root:               bulbous=b,club=c,cup=u,equal=e,
+                                  rhizomorphs=z,rooted=r,missing=?
+    12. stalk-surface-above-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    13. stalk-surface-below-ring: fibrous=f,scaly=y,silky=k,smooth=s
+    14. stalk-color-above-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,
+                                  pink=p,red=e,white=w,yellow=y
+    15. stalk-color-below-ring:   brown=n,buff=b,cinnamon=c,gray=g,orange=o,
+                                  pink=p,red=e,white=w,yellow=y
+    16. veil-type:                partial=p,universal=u
+    17. veil-color:               brown=n,orange=o,white=w,yellow=y
+    18. ring-number:              none=n,one=o,two=t
+    19. ring-type:                cobwebby=c,evanescent=e,flaring=f,large=l,
+                                  none=n,pendant=p,sheathing=s,zone=z
+    20. spore-print-color:        black=k,brown=n,buff=b,chocolate=h,green=r,
+                                  orange=o,purple=u,white=w,yellow=y
+    21. population:               abundant=a,clustered=c,numerous=n,
+                                  scattered=s,several=v,solitary=y
+    22. habitat:                  grasses=g,leaves=l,meadows=m,paths=p,
+                                  urban=u,waste=w,woods=d
+    23. class:                    edible=e, poisonous=p
 '''
 
 #Adathalmaz beolvasása
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
-iris_dataset = pd.read_csv('data/iris_dataset.txt',
+mushroom_dataset = pd.read_csv('data/agaricus-lepiota.txt',
                            sep=',',
-                           names=['sepal_len',
-                                  'sepal_wid',
-                                  'petal_len',
-                                  'petal_wid',
-                                  'class'])
-X = iris_dataset.drop('class',axis=1)
-X = StandardScaler().fit_transform(X)
-Y = iris_dataset['class']
+                           names=['class',
+                                  'cap-shape',
+                                  'cap-surface',
+                                  'cap-color',
+                                  'bruises',
+                                  'odor',
+                                  'gill-attachment',
+                                  'gill-spacing',
+                                  'gill-size',
+                                  'gill-color',
+                                  'stalk-shape',
+                                  'stalk-root',
+                                  'stalk-surface-above-ring',
+                                  'stalk-surface-below-ring',
+                                  'stalk-color-above-ring',
+                                  'stalk-color-below-ring',
+                                  'veil-type',
+                                  'veil-color',
+                                  'ring-number',
+                                  'ring-type',
+                                  'spore-print-color',
+                                  'population',
+                                  'habitat'])
+#Hiányzó adatokat tartalmazó rekordok eldobása
+mushroom_dataset = mushroom_dataset[mushroom_dataset['stalk-root'] != '?']
+#Oszlopok típusainak átkonvertálása
+for col in mushroom_dataset:
+    mushroom_dataset[col] = mushroom_dataset[col].astype('category')
+#Attribútumok leválasztása az osztálytól
+X = mushroom_dataset.drop('class',axis=1)
+cat_columns = X.select_dtypes(['category']).columns
+X[cat_columns] = X[cat_columns].apply(lambda x: x.cat.codes)
+Y = mushroom_dataset['class']
 
 #Dimenzióredukció végrehajtása az adathalmazon
 from sklearn.decomposition import PCA
@@ -33,28 +84,30 @@ pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(X)
 principalDf = pd.DataFrame(data = principalComponents
              , columns = ['principal component 1', 'principal component 2'])
-finalDf = pd.concat([principalDf, iris_dataset[['class']]], axis = 1)
+finalDf = pd.concat([principalDf, mushroom_dataset[['class']]], axis = 1)
 
 #A dimenzióredukció eredményének vizualizációja
 import matplotlib.pyplot as plt
+
+
 plt.figure(figsize = (6,6))
-plt.title('PCA az az Iris Dataset-en')
-targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-colors = ['r', 'g', 'b']
+plt.title('PCA a Mushroom data set-en')
+targets = ['p','e']
+colors = ['r', 'g']
 for target, color in zip(targets,colors):
     indicesToKeep = finalDf['class'] == target
     plt.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
                , finalDf.loc[indicesToKeep, 'principal component 2']
                , c = color
                , s = 50)
-plt.legend(targets)
+plt.legend(['mérgező','ehető'])
 plt.grid()
-plt.show()
 plt.savefig('data/pca.png')
+plt.show()
 plt.clf()
 
 #Keresett klaszterek számosságának beállítása
-K = 3
+K = 2
 #KMeans algoritmus használata az adathalmazon klaszterezésként
 from sklearn.cluster import KMeans
 
@@ -67,14 +120,14 @@ for i in range(0,K):
                 principalComponents[kmeans.labels_ == i][:, 1])
 plt.title('K-Means eredmény')
 plt.grid()
-plt.show()
 plt.savefig('data/kmeans.png')
+plt.show()
 plt.clf()
 
 #DBSCAN algoritmus használata az adathalmazon
 from sklearn.cluster import DBSCAN
 
-dbscan = DBSCAN(eps=1,min_samples=1).fit(X)
+dbscan = DBSCAN(eps=10,min_samples=1).fit(X)
 
 #DBSCAN eredmény megjelenítése scatter ploton
 plt.figure(figsize = (6,6))
@@ -83,6 +136,22 @@ for i in range(0,K):
                 principalComponents[dbscan.labels_ == i][:, 1])
 plt.title('DBSCAN eredmény')
 plt.grid()
-plt.show()
 plt.savefig('data/dbscan.png')
+plt.show()
+plt.clf()
+
+#Agglomerációs algoritmus használata az adathalmazon klaszterezésként
+from sklearn.cluster import AgglomerativeClustering
+
+agglomerative = AgglomerativeClustering(n_clusters=K).fit(X)
+
+#Agglomerációs klaszterezés eredmény megjelenítése scatter ploton
+plt.figure(figsize = (6,6))
+for i in range(0,K):
+    plt.scatter(principalComponents[agglomerative.labels_ == i][:, 0],
+                principalComponents[agglomerative.labels_ == i][:, 1])
+plt.title('Agglomerációs klaszterezés eredmény')
+plt.grid()
+plt.savefig('data/meanshift.png')
+plt.show()
 plt.clf()
